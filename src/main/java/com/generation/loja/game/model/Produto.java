d809d7c877d468ed673e48e4b8689efe312ad03a
Keyla <Keyladias.dk@hotmail.com>
2022-03-07 20:33:52 -0300

    Loja Game

--- src/main/java/com/generation/loja/game/model/Produto.java
+++ src/main/java/com/generation/loja/game/model/Produto.java
@@ -50,6 +50,10 @@ public class Produto {
     @ManyToOne
     @JsonIgnoreProperties("produto")
     private Categoria categoria;
+    
+    @ManyToOne
+    @JsonIgnoreProperties("postagem")
+    private Usuario usuario;
 
 	public Long getId() {
 		return id;
@@ -121,6 +125,14 @@ public class Produto {
 
 	public void setCategoria(Categoria categoria) {
 		this.categoria = categoria;
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
 	}
 
 }
---
+++ src/main/java/com/generation/loja/game/repository/UsuarioRepository.java
@@ -0,0 +1,20 @@
+package com.generation.loja.game.repository;
+
+import java.util.Optional;
+
+import com.generation.loja.game.model.Usuario;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+	/**
+	 * Método que busca um usuário pelo seu usuario (email).
+	 * 
+	 * select * from tb_usuarios where usuario = "usuario procurado"
+	 */
+	public Optional<Usuario> findByUsuario(String usuario);
+
+}
